b4c2c949957526274200cc4a24c8374f9e129eef
Tech33 <kakkar33@gmail.com>
2026-09-11 19:22:14 +0100

    chore: bump version to v1.6.12 (versionCode 54)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.portalhacks.frame"
         minSdk = 23
         targetSdk = 29
-        versionCode = 53
-        versionName = "1.6.11"
+        versionCode = 54
+        versionName = "1.6.12"
 
     }
 
