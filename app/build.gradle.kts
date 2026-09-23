04a01967924c1a7862ed95d067fcb8f64401ff47
Tech33 <kakkar33@gmail.com>
2026-09-24 00:20:42 +0100

    chore: bump version to v1.6.29 (code 71)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.portalhacks.frame"
         minSdk = 23
         targetSdk = 29
-        versionCode = 70
-        versionName = "1.6.28"
+        versionCode = 71
+        versionName = "1.6.29"
 
     }
 
